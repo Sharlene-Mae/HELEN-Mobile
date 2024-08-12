fa7e9c9c459b6d86e648e1e28b2cd2e1d98443d2
Sharlene-Mae <89128730+Sharlene-Mae@users.noreply.github.com>
2024-08-13 00:43:33 +0800

    Update profile-buyer.dart

--- lib/buyers/profile-buyer.dart
+++ lib/buyers/profile-buyer.dart
@@ -9,178 +9,142 @@ class ProfilePageBuyer extends StatelessWidget {
         child: Container(
           color: Colors.white,
           padding: EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                radius: 50.0,
-                backgroundColor: Colors.grey,
-                child: ClipOval(
-                  child: Image.asset(
-                    'images/buyers/sample-buyer-pic.jpg', // Updated image path
-                    width: 100.0,
-                    height: 100.0,
-                    fit: BoxFit.cover,
-                  ),
+          child: Container(
+            padding: EdgeInsets.all(20.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  blurRadius: 10,
+                  offset: Offset(0, 5),
                 ),
-              ),
-              SizedBox(height: 10.0),
-              Text(
-                '@jstn_bala',
-                style: TextStyle(
-                  fontFamily: 'Poppins',
-                  fontSize: 24.0,
-                  color: Color(0xFF0C7230),
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 20.0),
-              Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Color(0xFF0C7230),
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                padding: EdgeInsets.all(20.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+              ],
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
                   children: [
-                    Text(
-                      'Buyer Information',
-                      style: TextStyle(
-                        fontFamily: 'Poppins',
-                        fontSize: 24.0,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(height: 10.0),
-                    Text(
-                      'Full Name:',
-                      style: TextStyle(
-                        fontFamily: 'Poppins',
-                        fontSize: 14.0,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      'Justin Bala',
-                      style: TextStyle(
-                        fontFamily: 'Poppins',
-                        fontSize: 14.0,
-                        color: Colors.white,
-                      ),
-                    ),
-                    SizedBox(height: 10.0),
-                    Text(
-                      'Address:',
-                      style: TextStyle(
-                        fontFamily: 'Poppins',
-                        fontSize: 14.0,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      '123 Rizal Street, Barangay 5, Lucban, Quezon, Philippines 4328',
-                      style: TextStyle(
-                        fontFamily: 'Poppins',
-                        fontSize: 14.0,
-                        color: Colors.white,
-                      ),
-                    ),
-                    SizedBox(height: 10.0),
-                    Text(
-                      'Organization:',
-                      style: TextStyle(
-                        fontFamily: 'Poppins',
-                        fontSize: 14.0,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      'Organic Valley',
-                      style: TextStyle(
-                        fontFamily: 'Poppins',
-                        fontSize: 14.0,
-                        color: Colors.white,
-                      ),
-                    ),
-                    SizedBox(height: 10.0),
-                    Text(
-                      'Contact No:',
-                      style: TextStyle(
-                        fontFamily: 'Poppins',
-                        fontSize: 14.0,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      '09278557033',
-                      style: TextStyle(
-                        fontFamily: 'Poppins',
-                        fontSize: 14.0,
-                        color: Colors.white,
+                    Expanded(
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Column(
+                          children: [
+                            CircleAvatar(
+                              radius: 100.0,
+                              backgroundColor: Colors.grey,
+                              child: ClipOval(
+                                child: Image.asset(
+                                  'images/buyers/sample-buyer-pic.jpg', // Updated image path
+                                  width: 200.0,
+                                  height: 200.0,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 10.0),
+                            Text(
+                              '@jstn_bala',
+                              style: TextStyle(
+                                fontFamily: 'Poppins',
+                                fontSize: 20.0,
+                                color: Color(0xFFCA771A),
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              'Institutional Buyer',
+                              style: TextStyle(
+                                fontFamily: 'Poppins',
+                                fontSize: 16.0,
+                                color: Color(0xFF606060),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ],
                 ),
-              ),
-              SizedBox(height: 20.0),
-              ElevatedButton(
-                onPressed: () {
-                  // Add onPressed action here
-                },
-                child: Text(
-                  'Edit Profile',
-                  style: TextStyle(
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-                style: ElevatedButton.styleFrom(
-                  primary: Color(0xFF0C7230),
-                  padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 15.0),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0),
-                  ),
-                ),
-              ),
-              SizedBox(height: 20.0),
-              ElevatedButton(
-                onPressed: () {
-                  // Add onPressed action here
-                },
-                child: Row(
+                SizedBox(height: 20.0),
+                Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Text(
-                      'Pending Orders',
+                      'Personal Information',
                       style: TextStyle(
                         fontFamily: 'Poppins',
+                        fontSize: 20.0,
+                        color: Color(0xFFCA771A),
                         fontWeight: FontWeight.bold,
-                        color: Colors.white,
                       ),
                     ),
-                    Icon(
-                      Icons.arrow_drop_down,
-                      color: Colors.white,
+                    IconButton(
+                      icon: Icon(Icons.edit, color: Color(0xFFCA771A)),
+                      onPressed: () {
+                        // Add your edit action here
+                      },
                     ),
                   ],
                 ),
-                style: ElevatedButton.styleFrom(
-                  primary: Color(0xFF0C7230),
-                  padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
+                SizedBox(height: 10.0),
+                Text(
+                  'Full Name:',
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 14.0,
+                    color: Color(0xFFCA771A),
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-            ],
+                Text(
+                  'Justin Bala',
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 14.0,
+                    color: Color(0xFF606060),
+                  ),
+                ),
+                SizedBox(height: 10.0),
+                Text(
+                  'Address:',
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 14.0,
+                    color: Color(0xFFCA771A),
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  '123 Rizal Street, Barangay 5, Lucban, Quezon, Philippines 4328',
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 14.0,
+                    color: Color(0xFF606060),
+                  ),
+                ),
+                SizedBox(height: 10.0),
+                Text(
+                  'Contact No:',
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 14.0,
+                    color: Color(0xFFCA771A),
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  '09278557033',
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 14.0,
+                    color: Color(0xFF606060),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
