e57884e55a8a3db6f1266c39fc5583e805b8a669
Sharlene-Mae <89128730+Sharlene-Mae@users.noreply.github.com>
2024-08-13 00:41:35 +0800

    Update nav-bar-buyer.dart

--- lib/buyers/nav-bar-buyer.dart
+++ lib/buyers/nav-bar-buyer.dart
@@ -1,22 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:helen_app/buyers/help-buyer.dart';
 import 'package:helen_app/buyers/login-buyer.dart';
-import 'package:helen_app/buyers/previousorders.dart';
 import 'homepage-buyer.dart';
 import 'messages-buyer.dart';
 import 'orderlists-buyer.dart';
 import 'profile-buyer.dart';
-import 'about-buyer.dart'; // Import the AboutBuyer class
+import 'about-buyer.dart'; 
 
 class NavbarBuyer extends StatefulWidget {
+  final int initialIndex;
+
+  const NavbarBuyer({Key? key, this.initialIndex = 0}) : super(key: key);
+
   @override
   _NavbarBuyerState createState() => _NavbarBuyerState();
 }
 
 class _NavbarBuyerState extends State<NavbarBuyer> {
-  int _selectedIndex = 0;
+  late int _selectedIndex;
 
-  static const Color selectedColor = Color(0xFF0C7230);
+  @override
+  void initState() {
+    super.initState();
+    _selectedIndex = widget.initialIndex;
+  }
+
+  // Selected Color for Bottom Nav Bar
+  static const Color selectedColor = Color(0xFFCA771A);
   static const Color unselectedColor = Color(0xFF606060);
 
   void _onItemTapped(int index) {
@@ -28,18 +38,18 @@ class _NavbarBuyerState extends State<NavbarBuyer> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: HalfWhiteDrawer(), // Adding the drawer
+      drawer: const HalfWhiteDrawer(), // Adding the drawer
       body: Column(
         children: [
           Stack(
             children: [
               ClipRRect(
-                borderRadius: BorderRadius.only(
+                borderRadius: const BorderRadius.only(
                   bottomLeft: Radius.circular(30),
                   bottomRight: Radius.circular(30),
                 ),
                 child: Container(
-                  color: Color(0xFF0C7230),
+                  color: const Color(0xFFCA771A),
                   width: double.infinity,
                   height: 150,
                 ),
@@ -58,7 +68,7 @@ class _NavbarBuyerState extends State<NavbarBuyer> {
                 left: 10,
                 child: Builder(
                   builder: (context) => IconButton(
-                    icon: Icon(
+                    icon: const Icon(
                       Icons.menu,
                       color: Colors.white,
                     ),
@@ -74,7 +84,7 @@ class _NavbarBuyerState extends State<NavbarBuyer> {
                 child: IconButton(
                   icon: Stack(
                     children: [
-                      Icon(
+                      const Icon(
                         Icons.notifications,
                         color: Colors.white,
                         size: 30,
@@ -82,16 +92,16 @@ class _NavbarBuyerState extends State<NavbarBuyer> {
                       Positioned(
                         right: 0,
                         child: Container(
-                          padding: EdgeInsets.all(1),
+                          padding: const EdgeInsets.all(1),
                           decoration: BoxDecoration(
                             color: Colors.red,
                             borderRadius: BorderRadius.circular(6),
                           ),
-                          constraints: BoxConstraints(
+                          constraints: const BoxConstraints(
                             minWidth: 12,
                             minHeight: 12,
                           ),
-                          child: Text(
+                          child: const Text(
                             '0',
                             style: TextStyle(
                               color: Colors.white,
@@ -146,11 +156,11 @@ class _NavbarBuyerState extends State<NavbarBuyer> {
         selectedItemColor: selectedColor,
         unselectedItemColor: unselectedColor,
         onTap: _onItemTapped,
-        selectedLabelStyle: TextStyle(
+        selectedLabelStyle: const TextStyle(
           color: selectedColor,
           fontFamily: 'Poppins',
         ),
-        unselectedLabelStyle: TextStyle(
+        unselectedLabelStyle: const TextStyle(
           color: unselectedColor,
           fontFamily: 'Poppins',
         ),
@@ -161,144 +171,124 @@ class _NavbarBuyerState extends State<NavbarBuyer> {
   }
 }
 
+
+// Drawer for Buyer's Profile
 class HalfWhiteDrawer extends StatelessWidget {
+  const HalfWhiteDrawer({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Padding(
-        padding: const EdgeInsets.only(top: 30.0), // Adjust the top padding as needed
-        child: Container(
-          color: Colors.white,
-          width: MediaQuery.of(context).size.width / 2, // Adjust width as needed
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                padding: EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Buyer's Name",
-                      style: TextStyle(
-                        color: Color(0xFF0C7230),
-                        fontFamily: 'Poppins',
-                        fontWeight: FontWeight.bold,
-                        fontSize: 30.0,
-                      ),
-                    ),
-                    SizedBox(height: 40.0), // Added extra space
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => PreviousOrdersBuyer()),
-                        );
-                      },
-                      child: Row(
-                        children: [
-                          Icon(Icons.history, color: Color(0xFF0C7230), size: 30.0,),
-                          SizedBox(width: 12.0), // Increased spacing
-                          Text(
-                            'Previous Orders',
-                            style: TextStyle(
-                              color: Color(0xFF0C7230),
-                              fontFamily: 'Poppins',
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20.0,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(height: 20.0), // Added extra space
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => AboutBuyer()),
-                        );
-                      },
-                      child: Row(
-                        children: [
-                          Icon(Icons.info, color: Color(0xFF0C7230), size: 30.0,),
-                          SizedBox(width: 12.0), // Increased spacing
-                          Text(
-                            'About',
-                            style: TextStyle(
-                              color: Color(0xFF0C7230),
-                              fontFamily: 'Poppins',
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20.0,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(height: 20.0), // Added extra space
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => HelpBuyerScreen()),
-                        );
-                      },
-                      child: Row(
-                        children: [
-                          Icon(Icons.help, color: Color(0xFF0C7230), size: 30.0,),
-                          SizedBox(width: 12.0), // Increased spacing
-                          Text(
-                            'Help',
-                            style: TextStyle(
-                            color: Color(0xFF0C7230),
-                            fontFamily: 'Poppins',
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20.0,
-                          ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Spacer(),
-              Align(
-                alignment: Alignment.bottomRight,
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                     Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => LoginPageBuyer()),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      primary: Color(0xFF0C7230),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8.0),
-                      ),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 12.0,
-                        horizontal: 20.0,
-                      ),
-                      child: Text(
-                        'Logout',
+      child: Container(
+        color: Colors.white, // Ensure the background color is white
+        width: MediaQuery.of(context).size.width / 2, // Adjust width as needed
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Drawer Header with Image and Buyer Info
+            Container(
+             padding: const EdgeInsets.only(
+            top: 50.0, // Top padding
+            left: 50.0, // Side padding (left)
+            ),
+              child: const Column(
+                children: [
+                  // Profile Image
+                  CircleAvatar(
+                    backgroundImage: AssetImage('images/buyers/sample-buyer-pic.jpg'),
+                    radius: 100,
+                  ),
+                  SizedBox(height: 10.0),
+                  // Buyer Info
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Justin T. Bala", // This will be replaced with the actual buyer's name
                         style: TextStyle(
-                          color: Colors.white,
+                          color: Color(0xFFCA771A),
                           fontFamily: 'Poppins',
                           fontWeight: FontWeight.bold,
+                          fontSize: 20.0,
                         ),
                       ),
-                    ),
+                      Text(
+                        'Institutional Buyer', // This will change based on account type
+                        style: TextStyle(
+                          color: Color(0xFF606060),
+                          fontFamily: 'Poppins',
+                          fontSize: 16.0,
+                        ),
+                      ),
+                    ],
                   ),
-                ),
+                ],
               ),
-            ],
-          ),
+            ),
+            const SizedBox(height: 20.0),
+            _drawerItem(
+              context,
+              icon: Icons.info,
+              text: 'About',
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => AboutBuyer()),
+                );
+              },
+            ),
+            const SizedBox(height: 5.0),
+            _drawerItem(
+              context,
+              icon: Icons.help,
+              text: 'Help',
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => HelpBuyerScreen()),
+                );
+              },
+            ),
+            const SizedBox(height: 20.0),
+            const Divider(), // Line separator
+            _drawerItem(
+              context,
+              icon: Icons.exit_to_app,
+              text: 'Logout',
+              onTap: () {
+                // Handle logout functionality
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginPageBuyer()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Helper method to create drawer items
+  Widget _drawerItem(BuildContext context, {required IconData icon, required String text, required VoidCallback onTap}) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 16.0),
+        child: Row(
+          children: [
+            Icon(icon, color: const Color(0xFFCA771A), size: 30.0),
+            const SizedBox(width: 12.0),
+            Text(
+              text,
+              style: const TextStyle(
+                color: Color(0xFFCA771A),
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.bold,
+                fontSize: 20.0,
+              ),
+            ),
+          ],
         ),
       ),
     );
